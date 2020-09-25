5a0c481e61e867469b9a319eae733db027c55df2
gguir <gguir@10.137.80.2>
2020-09-25 14:39:55 -0300

    Inicio de projeto

---
+++ src/main/java/com/reidnet/curso/boot/dao/DepartamentoDaoImpl.java
@@ -0,0 +1,10 @@
+package com.reidnet.curso.boot.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.reidnet.curso.boot.domain.Departamento;
+
+@Repository
+public class DepartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao {
+
+}
